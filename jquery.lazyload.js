56f8d84d84c19503eb12be6e086833c12074acf0
Will Strootman <wstrootman@rightsourcecompliance.com>
2017-12-08 11:59:34 -0600

    Working image replace and following 2 images

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -18,6 +18,7 @@
 
     $.fn.lazyload = function(options) {
         var elements = this;
+        var allElements = this;
         var $container;
         var settings = {
             threshold       : 0,
@@ -33,6 +34,8 @@
             placeholder     : "data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs="
         };
 
+        var timeoutId = -1;
+
         function update() {
             var counter = 0;
 
@@ -43,12 +46,12 @@
                 }
                 if ($.abovethetop(this, settings) ||
                     $.leftofbegin(this, settings)) {
-                        /* Nothing. */
+                    /* Nothing. */
                 } else if (!$.belowthefold(this, settings) &&
                     !$.rightoffold(this, settings)) {
-                        $this.trigger("appear");
-                        /* if we found an image we'll load, reset the counter */
-                        counter = 0;
+                    $this.trigger("appear");
+                    /* if we found an image we'll load, reset the counter */
+                    counter = 0;
                 } else {
                     if (++counter > settings.failure_limit) {
                         return false;
@@ -74,12 +77,17 @@
 
         /* Cache container as jQuery as object. */
         $container = (settings.container === undefined ||
-                      settings.container === window) ? $window : $(settings.container);
+            settings.container === window) ? $window : $(settings.container);
 
         /* Fire one scroll event per scroll. Not one scroll event per image. */
         if (0 === settings.event.indexOf("scroll")) {
             $container.off(settings.event).on(settings.event, function() {
-                return update();
+                if (0 <= timeoutId) {
+                    clearTimeout(timeoutId);
+                }
+                timeoutId = setTimeout(function(){
+                    update();
+                }, 100);
             });
         }
 
@@ -96,9 +104,8 @@
                 }
             }
 
-            /* When appear is triggered load original image. */
-            $self.one("appear", function() {
-                if (!this.loaded) {
+            var showImage = function() {
+                if (!self.loaded) {
                     if (settings.appear) {
                         var elements_left = elements.length;
                         settings.appear.call(self, elements_left, settings);
@@ -118,8 +125,12 @@
                                 } if ($self.is("video")) {
                                     $self.attr("poster", original);
                                 } else {
-                                    $self.css("background-image", "url('" + original + "')");
-                                    $self.css("background-position", null);
+                                    $self.css({
+                                        "background-image": "url('" + original + "')",
+                                        "background-position": "0px 0px"//,
+                                        //"background-repeat": "",
+                                        //"background-size": ""
+                                    });
                                 }
                                 $self[settings.effect](settings.effect_speed);
                             }
@@ -127,10 +138,10 @@
                             self.loaded = true;
 
                             /* Remove image from array so it is not looped next time. */
-                            var temp = $.grep(elements, function(element) {
-                                return !element.loaded;
-                            });
-                            elements = $(temp);
+                            // var temp = $.grep(elements, function(element) {
+                            //     return !element.loaded;
+                            // });
+                            // elements = $(temp);
 
                             if (settings.load) {
                                 var elements_left = elements.length;
@@ -145,6 +156,25 @@
                             "srcset": $self.attr("data-" + settings.data_srcset) || ""
                         });
                 }
+            };
+
+            /* When appear is triggered load original image. */
+            $self.one("appear", function() {
+                // show thyself
+                showImage();
+                // find position and trigger cascade load on neighbors
+                var pos = allElements.index(self);
+                setTimeout(function() {
+                    for (var i=pos, imax=Math.min(allElements.length, pos+3); i>-1 && i<imax; i++) {
+                        var neighbor = allElements[i];
+                        $(neighbor).trigger("appear-cascade");
+                    }
+                }, 500);
+            });
+
+            /* When a nearby image (above?) is triggered, also load this one's original image. */
+            $self.one("appear-cascade", function() {
+                showImage();
             });
 
             /* When wanted event is triggered load original image */
@@ -235,9 +265,9 @@
     };
 
     $.inviewport = function(element, settings) {
-         return !$.rightoffold(element, settings) && !$.leftofbegin(element, settings) &&
-                !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
-     };
+        return !$.rightoffold(element, settings) && !$.leftofbegin(element, settings) &&
+            !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
+    };
 
     /* Custom selectors for your convenience.   */
     /* Use as $("img:below-the-fold").something() or */
